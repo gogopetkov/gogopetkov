d0c32c161765c1a285cf40cc195a45055e4867c1
gogopetkov <georgi_petkov@hotmail.com>
2024-02-11 13:09:34 +0200

    apps

--- blink/blink.c
+++ blink/blink.c
@@ -9,7 +9,7 @@ int gpio_blink_main(void* p) {
         furi_hal_gpio_write(&gpio_ext_pa7, true); //LED on
         furi_delay_ms(100);
         furi_hal_gpio_write(&gpio_ext_pa7, false); //LED off
-        furi_delay_ms(100);
+        furi_delay_ms(2000);
     } while(furi_hal_gpio_read(&gpio_button_back));
 
     furi_hal_gpio_init_simple(&gpio_ext_pa7, GpioModeAnalog);
